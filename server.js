d80a8af6422a13090ffafe5f5d32036a68c50f48
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-10-25 19:17:32 -0400

    send data from client to server side and save it into an object

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
 const app = express();
 
@@ -15,10 +16,22 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
 // Setup Server
 app.listen(3000, () => {
   console.log('listening on port 3000');
+});
+
+// Routes
+app.get('/weather', (req, res) => {
+  console.log('get');
+  res.send(projectData);
+});
+
+app.post('/weather', (req, res) => {
+  projectData = { ...req.body };
+  res.send('post');
 });
--- website/app.js
+++ website/app.js
@@ -3,15 +3,38 @@ const API_KEY = '4c78cc8e478223d16b16f9939ae05c6a';
 const UNITS = 'imperial';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 const url = `${BASE_URL}zip=94040,us&appid=${API_KEY}&units=${UNITS}`;
+const feelings = document.getElementById('feelings');
+const generateBtn = document.getElementById('generate');
+const projectData = {};
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
-const getWeatherData = async (url) => {
+const gatherData = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.main.temp);
+  projectData.tempertaure = data.main.temp;
+  projectData.date = newDate;
+  projectData.feelings = feelings.value;
+  console.log(projectData);
+  return projectData;
 };
 
-getWeatherData(url);
+const postData = async (url, data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({
+      tempertaure: data.tempertaure,
+      date: data.date,
+      feelings: data.feelings,
+    }),
+  });
+};
+
+generateBtn.addEventListener('click', () => {
+  gatherData(url).then((response) => {
+    postData('/weather', response);
+  });
+});
